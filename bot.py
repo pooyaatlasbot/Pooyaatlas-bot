fca96dbb8dcd689dc283e21566f18f12cb8db7ae
pooyaatlasbot <mrsheikhali2021@gmail.com>
2026-07-18 20:37:15 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ from telegram.ext import (
 )
 
 from handlers.start import start
+from handlers.register import register_handler
 from database import create_database
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -13,14 +14,18 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 def main():
 
-    if BOT_TOKEN is None:
-        raise ValueError("BOT_TOKEN پیدا نشد.")
+    if not BOT_TOKEN:
+        raise ValueError("BOT_TOKEN تنظیم نشده است.")
 
     create_database()
 
     app = Application.builder().token(BOT_TOKEN).build()
 
+    # دستور استارت
     app.add_handler(CommandHandler("start", start))
+
+    # فرم ثبت نام
+    app.add_handler(register_handler)
 
     print("✈️ Pooya Flight Registration Bot Started")
 
