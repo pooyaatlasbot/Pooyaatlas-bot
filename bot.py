e6767ca88b6d7317e60dddd8ed591982ac311a4d
pooyaatlasbot <mrsheikhali2021@gmail.com>
2026-07-18 20:11:36 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,44 +1,28 @@
 import os
 
-from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import (
     Application,
     CommandHandler,
-    ContextTypes,
 )
+
+from handlers.start import start
+from database import create_database
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    keyboard = [
-        ["✈️ ثبت نام دوره‌ها"],
-        ["📄 قرارداد آموزشی"],
-        ["💰 شهریه دوره‌ها"],
-        ["📞 تماس با مشاور"],
-    ]
-
-    reply_markup = ReplyKeyboardMarkup(
-        keyboard,
-        resize_keyboard=True
-    )
-
-    await update.message.reply_text(
-        "👋 به سامانه ثبت‌نام آموزشگاه خلبانی پویا خوش آمدید.\n\n"
-        "لطفاً یکی از گزینه‌های زیر را انتخاب کنید.",
-        reply_markup=reply_markup
-    )
-
-
 def main():
+
     if not BOT_TOKEN:
         raise ValueError("BOT_TOKEN تنظیم نشده است.")
+
+    create_database()
 
     app = Application.builder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
 
-    print("✅ Bot Started...")
+    print("✈️ Pooya Flight Registration Bot Started")
 
     app.run_polling()
 
