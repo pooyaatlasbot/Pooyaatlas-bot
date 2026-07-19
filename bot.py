3c9445cbab5793f8da5ac90cf84ff7fb7d2dcfe5
pooyaatlasbot <mrsheikhali2021@gmail.com>
2026-07-19 12:10:42 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -48,6 +48,27 @@ def main():
         contract_handler
     )
 
+# منوی اصلی
+app.add_handler(
+    MessageHandler(
+        filters.Regex("^📞 تماس با مشاور$"),
+        menu,
+    )
+)
+
+app.add_handler(
+    MessageHandler(
+        filters.Regex("^💰 شهریه دوره‌ها$"),
+        menu,
+    )
+)
+
+app.add_handler(
+    MessageHandler(
+        filters.Regex("^📄 قرارداد آموزشی$"),
+        menu,
+    )
+)
     # دریافت قرارداد امضا شده
     app.add_handler(
         MessageHandler(
